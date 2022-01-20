af61a9a4c9e724dd18383fc9bfaa3ec74aae715c
lucasrmoro <lucas.rmoro1@gmail.com>
2022-01-19 23:10:27 -0300

    Creating the Network Module to DI

---
+++ app/src/main/java/br/com/lucas/githubrepositories/di/NetworkModule.kt
@@ -0,0 +1,23 @@
+package br.com.lucas.githubrepositories.di
+
+import br.com.lucas.githubrepositories.network.GitHubApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideGitHubApi(): GitHubApi = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(GitHubApi::class.java)
+}
