289dfc29b6bc80db3f4f0886ebe7486ab7fa0452
lucasrmoro <lucas.rmoro1@gmail.com>
2022-01-19 23:45:48 -0300

    Modifying the kind of returning on GitHubApi

--- app/src/main/java/br/com/lucas/githubrepositories/network/GitHubApi.kt
+++ app/src/main/java/br/com/lucas/githubrepositories/network/GitHubApi.kt
@@ -1,11 +1,11 @@
 package br.com.lucas.githubrepositories.network
 
 import br.com.lucas.githubrepositories.data.model.Repository
-import kotlinx.coroutines.flow.Flow
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitHubApi {
     @GET("users/{user}/repos")
-    suspend fun listOfRepositories(@Path("user") user: String): Flow<Repository>
+    suspend fun listOfRepositories(@Path("user") user: String): Call<List<Repository>>
 }
