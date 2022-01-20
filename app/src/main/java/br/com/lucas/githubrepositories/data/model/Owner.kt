ccee71f83b2d520951b1fbd6b34122cdc9145098
lucasrmoro <lucas.rmoro1@gmail.com>
2022-01-19 23:02:42 -0300

    Creating the model

---
+++ app/src/main/java/br/com/lucas/githubrepositories/data/model/Owner.kt
@@ -0,0 +1,8 @@
+package br.com.lucas.githubrepositories.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Owner(
+    val login: String,
+    @SerializedName("avatar_url") val avatarURL: String
+)
---
+++ app/src/main/java/br/com/lucas/githubrepositories/data/model/Repository.kt
@@ -0,0 +1,13 @@
+package br.com.lucas.githubrepositories.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Repository(
+    val id: Long,
+    val name: String,
+    val owner: Owner,
+    @SerializedName("html_url") val htmlURL: String,
+    val description: String,
+    @SerializedName("stargazers_count") val stargazersCount: Long,
+    val language: String
+)
